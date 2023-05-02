006f6ee0c633d11c419154c015932f64b488ddb7
Eric German <german.eric@gmail.com>
2023-05-02 11:17:50 +0200

    Update exo5.py

--- blog/exo5.py
+++ blog/exo5.py
@@ -1,17 +1,21 @@
 # construire une classe qui prend en argument dans son constructeur une chaine de caractere 
 # et retourne une liste de tuple avec le mot et son comteur d'apparition dans le texte.
 # exemple :  Comptage('Pourquoi , pourquoi , as tu fait ca, tu n'est pas un as de la route')
+# regle de gestion : 
+#  - ne tenir compte d'un mot d'une lettre
+#  - ne pas compter ce qui est avant un apostrophe
 
 class Comptage():
     def __init__(self, texte):
         self.dico = {}
         tab_texte = texte.split(' ')
-        for mot in tab_texte:
-            if mot != '':
-                if mot.lower() in self.dico:
-                    self.dico[mot.lower()] += 1
-                else:
-                    self.dico[mot.lower()] = 1
+        for grpmot in tab_texte:
+            for smot in sousmot:
+                if smot != '' and len(mot) > 1:
+                  if mot.lower() in self.dico:
+                      self.dico[mot.lower()] += 1
+                  else:
+                      self.dico[mot.lower()] = 1
 
     def liste(self):
         liste_ = []
