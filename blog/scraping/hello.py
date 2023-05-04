d6fce0dbbb401a5f96e00891ab41835e1e11650c
Eric German <german.eric@gmail.com>
2023-05-04 08:49:49 +0200

    manage erreur 404

--- blog/scraping/hello.py
+++ blog/scraping/hello.py
@@ -1,8 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 200
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)    
+    app.run(host='0.0.0.0', port=8080)    # change port in production
+    
