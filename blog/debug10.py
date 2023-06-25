b125ec02e4be4e7801318b02b92e06618932af7e
Eric German <german.eric@gmail.com>
2023-06-25 11:02:49 +0200

    debug

---
+++ blog/debug10.py
@@ -0,0 +1,6 @@
+a = [5, 6, 1, 3, 2]
+a.sort()
+print(a)
+b = [5, 6, 1,'b', 3, 2]
+b.sort()
+print(b)
---
+++ blog/debug9.py
@@ -0,0 +1,6 @@
+a = []
+for n in range(2,20,2):
+    if n % 4 == 0:
+        a.append(n * 2)
+print('for', a) #for [8, 16, 24, 32]
+
