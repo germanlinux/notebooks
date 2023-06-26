31d4371ce61d3aa3263a44cc61acb4ca2989799d
Eric German <german.eric@gmail.com>
2023-06-26 19:21:12 +0200

    serious

---
+++ blog/debug11.py
@@ -0,0 +1,5 @@
+def volume(longueur , largeur, hauteur = 1):
+    return longueur * largeur * hauteur
+
+print(volume(3, hauteur = 2, largeur = 1,))
+print(volume(hauteur = 6, 3, largeur = 2))
---
+++ blog/debug12.py
@@ -0,0 +1,10 @@
+a = (4, 5, 4)
+longeur = a[0]
+largeur = a[1]
+hauteur = a[2]
+
+def volume(L,l,h):
+    return L * l * h 
+
+print(volume(longeur,largeur, hauteur))    
+print(volume(*a) )  
